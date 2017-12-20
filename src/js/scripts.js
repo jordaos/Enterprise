c9f52491964a128451a8e7a142c30295089ef023
Jordão Macedo <jordao05@hotmail.com>
2017-12-20 16:06:38 -0300

    Add form validation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,17 @@
 var app = angular.module('myApp', []);
 
-function LoginController($scope) {
-    
+function LoginController($scope, $timeout) {
+    $scope.isLoading = false;
+    $scope.loginInvalid = false;
+
+    $scope.login = function() {
+        $scope.isLoading = true;
+        $scope.loginInvalid = true;
+        
+        $timeout(function () {
+            $scope.isLoading = false;
+        }, 3000);
+    }
 }
 
 app.controller('login', LoginController);

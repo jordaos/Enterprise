8e22c5fe3767ef16f3509ecbe38586d8e91690a9
Jordão Macedo <jordao05@hotmail.com>
2017-12-20 16:54:56 -0300

    Add form effects in validation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,14 +3,26 @@ var app = angular.module('myApp', []);
 function LoginController($scope, $timeout) {
     $scope.isLoading = false;
     $scope.loginInvalid = false;
+    $scope.error = {message: '', show: false}
 
     $scope.login = function() {
-        $scope.isLoading = true;
-        $scope.loginInvalid = true;
-        
-        $timeout(function () {
-            $scope.isLoading = false;
-        }, 3000);
+        if ($scope.loginForm.$valid) {
+            $scope.isLoading = true;
+            $scope.error = {message: 'Login e/ou senha inválido(s).', show: true};
+
+            $timeout(function () {
+                $scope.isLoading = false;
+
+                $timeout(function () {
+                    $scope.error = {message: '', show: false}
+                }, 3000);
+            }, 3000);
+        } else {
+            $scope.error = {message: 'Preencha os campos corretamente!', show: true};
+            $timeout(function () {
+                $scope.error = {message: '', show: false}
+            }, 3000);
+        }
     }
 }
 

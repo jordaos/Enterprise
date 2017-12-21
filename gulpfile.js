f7d360212feed27419de1774dbf43291161d91de
Jordão Macedo <jordao05@hotmail.com>
2017-12-21 15:27:06 -0300

    Config gulp and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    nodemon = require('gulp-nodemon'),
+    notify = require("gulp-notify"),
+    browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
-    gulp.start(['sass', 'sass:watch']);
+    gulp.start(['serve']);
 })
 
 gulp.task('sass', function () {
     return gulp.src('./src/sass/**/*.sass')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
     });
-   
-gulp.task('sass:watch', function () {
+
+// Static server
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+
     gulp.watch('./src/sass/**/*.sass', ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app      = express();
 
 app.use(express.static(__dirname + '/'));
 
-app.listen(3000);
+//app.listen(3000);
 console.log("App listening on port 3000");
